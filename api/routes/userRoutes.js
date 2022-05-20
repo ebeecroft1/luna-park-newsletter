dacd650a625a17669fb9b7001333189451e77720
ebeecroft1 <eric.beecroft@gmail.com>
2022-05-20 14:03:52 +1000

    merge conflicts cleared

--- api/routes/userRoutes.js
+++ api/routes/userRoutes.js
@@ -4,9 +4,5 @@ module.exports = (app) => {
     app
         .route('/users')
         .get(userBuilder.listAllUsers) // Admin view for dashboard
-<<<<<<< HEAD
         .post(userBuilder.createUser); // User sign up
-=======
-        .post(userBuilder.createUser); // User sign up      
->>>>>>> e86f507df24376b24ecafae556ff0e006a1e7a54
 };
